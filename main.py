c1e981e78586dc58d15542425339241f336a6ea6
Max Schild <max@deintv.de>
2023-10-05 23:23:09 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import certifi as certifi
+import motor as motor
+#import pymongo as pymongo
+from fastapi import FastAPI
+import motor.motor_asyncio
+
+
+
+
+app = FastAPI()
+
+username = "mongo"
+password = "mongo"
+mongo_db_connection_string = "mongodb+srv://" \
+                             "{username}:{password}@cluster0.gvyd35q.mongodb.net/?retryWrites=true" \
+                             "&w=majority".format(username=username, password=password)
+#client = pymongo.MongoClient(mongo_db_connection_string)
+
+#client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+
+#trying somehting new
+#tlsCAFile=certifi.where()
+#client = motor.motor_asyncio.AsyncIOMotorClient(mongo_db_connection_string)
+client = motor.motor_asyncio.AsyncIOMotorClient(mongo_db_connection_string, tlsCAFile=certifi.where())
+db = client.sample_training
+
+@app.get("/")
+async def root():
+
+    #db = client.sample_training
+    #collection = await db.companies
+    #entry = await collection.find_one()
+    #entry = await db["students"].find_one().to_list(1000)
+    #entry = await db["companies"].find_one()
+    entry = await db["companies"].find().to_list(1000)
+    print(entry)
+
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
